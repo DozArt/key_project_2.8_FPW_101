9a41f52729cd61fd01657725ff5bf6d4f21e84cc
DozArt <wpeace@mail.ru>
2022-12-28 23:09:36 +0300

    Добавил краткое описание к заданию

--- main (1).py
+++ main (1).py
@@ -1,8 +1,3 @@
-# Код очень похож на тот, что предоставлен в разборое
-# но это лишь на первый взгляд
-# Здесь я подправил динамическое поле (от 5 до 9)
-# и переоформил отображение полей слева и справа
-# надеюсь исправил все ошибки
 from random import randint
 
 
@@ -10,10 +5,10 @@ class Dot:  # точка
     def __init__(self, x, y):
         self.x = x
         self.y = y
-
+    
     def __eq__(self, other):
         return self.x == other.x and self.y == other.y
-
+    
     def __repr__(self):  # строковое представление точки?
         return f"({self.x}, {self.y})"
 
@@ -42,24 +37,24 @@ class Ship:
         self.l = l
         self.o = o
         self.lives = l
-
+    
     @property
     def dots(self):
         ship_dots = []
         for i in range(self.l):
-            cur_x = self.bow.x
+            cur_x = self.bow.x 
             cur_y = self.bow.y
-
+            
             if self.o == 0:
                 cur_x += i
-
+            
             elif self.o == 1:
                 cur_y += i
-
+            
             ship_dots.append(Dot(cur_x, cur_y))
         # print(ship_dots)
         return ship_dots
-
+    
     # def shooten(self, shot):
     #     return shot in self.dots
 
@@ -68,26 +63,26 @@ class Board:
     def __init__(self, hid=False, size=6):
         self.size = size
         self.hid = hid
-
+        
         self.count = 0
-
-        self.field = [["O"] * size for _ in range(size)]
-
+        
+        self.field = [["O"]*size for _ in range(size)]
+        
         self.busy = []
         self.ships = []
 
     def add_ship(self, ship):
-
+        
         for d in ship.dots:
             if self.out(d) or d in self.busy:
                 raise BoardWrongShipException()
         for d in ship.dots:
             self.field[d.x][d.y] = "■"
             self.busy.append(d)
-
+        
         self.ships.append(ship)
         self.contour(ship)
-
+            
     def contour(self, ship, verb=False):
         near = [
             (-1, -1), (-1, 0), (-1, 1),
@@ -97,19 +92,19 @@ class Board:
         for d in ship.dots:
             for dx, dy in near:
                 cur = Dot(d.x + dx, d.y + dy)
-                if not (self.out(cur)) and cur not in self.busy:
+                if not(self.out(cur)) and cur not in self.busy:
                     if verb:
                         self.field[cur.x][cur.y] = "·"
                     self.busy.append(cur)
-
+    
     def __str__(self):
         res = " "
         for a in range(self.size):  # Изменил нумерацию верхней строки
             res += "  " + str(a + 1)
         res += " |"
         for i, row in enumerate(self.field):
-            res += f"\n{i + 1}| " + "  ".join(row) + " |"
-
+            res += f"\n{i+1}| " + "  ".join(row) + " |"
+        
         if self.hid:
             res = res.replace("■", "O")
         return res
@@ -122,21 +117,21 @@ class Board:
         for i, row in enumerate(self.field):
             res.append(f"{i + 1}| " + "  ".join(row) + " |")
             if self.hid:
-                res[i + 1] = res[i + 1].replace("■", "O")
+                res[i+1] = res[i+1].replace("■", "O")
         return res
 
     def out(self, d):
-        return not ((0 <= d.x < self.size) and (0 <= d.y < self.size))
+        return not((0 <= d.x < self.size) and (0 <= d.y < self.size))
 
     def shot(self, d):
         if self.out(d):
             raise BoardOutException()
-
+        
         if d in self.busy:
             raise BoardUsedException()
-
+        
         self.busy.append(d)
-
+        
         for ship in self.ships:
             if d in ship.dots:
                 ship.lives -= 1
@@ -149,11 +144,11 @@ class Board:
                 else:
                     print("Корабль ранен!")
                     return True
-
+        
         self.field[d.x][d.y] = "·"
         print("Мимо!")
         return False
-
+    
     def begin(self):
         self.busy = []
 
@@ -162,10 +157,10 @@ class Player:
     def __init__(self, board, enemy):
         self.board = board
         self.enemy = enemy
-
+    
     def ask(self):
         raise NotImplementedError()
-
+    
     def move(self):
         while True:
             try:
@@ -178,9 +173,9 @@ class Player:
 
 class AI(Player):
     def ask(self):  # добавляем elf.enemy.size чтобы AI знал размер поля
-        d = Dot(randint(0, self.enemy.size - 1), randint(0, self.enemy.size - 1))
+        d = Dot(randint(0, self.enemy.size-1), randint(0, self.enemy.size-1))
 
-        print(f"Ход компьютера: {d.x + 1} {d.y + 1}")
+        print(f"Ход компьютера: {d.x+1} {d.y+1}")
         return d
 
 
@@ -188,20 +183,20 @@ class User(Player):
     def ask(self):
         while True:
             cords = input("Ваш ход: ").split()
-
+            
             if len(cords) != 2:
                 print(" Введите 2 координаты! ")
                 continue
-
+            
             x, y = cords
-
-            if not (x.isdigit()) or not (y.isdigit()):
+            
+            if not(x.isdigit()) or not(y.isdigit()):
                 print(" Введите числа! ")
                 continue
-
+            
             x, y = int(x), int(y)
-
-            return Dot(x - 1, y - 1)
+            
+            return Dot(x-1, y-1)
 
 
 class Game:
@@ -212,13 +207,13 @@ class Game:
         co.hid = True
         self.ai = AI(co, pl)
         self.us = User(pl, co)
-
+    
     def random_board(self):
         board = None
         while board is None:
             board = self.random_place()
         return board
-
+    
     def random_place(self):
         lens = [3, 2, 2, 1, 1, 1, 1]
         board = Board(size=self.size)
@@ -260,7 +255,7 @@ class Game:
         s = self.us.board.size  # размер матрицы
         num = 0
         while True:
-            print("-" * 6 * (s + 2))  # Высшая математика количество дефисов
+            print("-"*6*(s+2))  # Высшая математика количество дефисов
             print(f"Доска пользователя: {'   ' * (s - 5)}    Доска компьютера:")  # динамический пробел
             for i in range(s + 1):
                 print(f"{self.us.board.split()[i]}      {self.ai.board.split()[i]}")
@@ -270,27 +265,27 @@ class Game:
             # print("Доска компьютера:")
             # print(self.ai.board)
             if num % 2 == 0:
-                print("-" * 6 * (s + 2))
+                print("-"*6*(s+2))
                 print("Ходит пользователь!")
                 repeat = self.us.move()
             else:
-                print("-" * 20)
+                print("-"*20)
                 print("Ходит компьютер!")
                 repeat = self.ai.move()
             if repeat:
                 num -= 1
-
+            
             if self.ai.board.count == 7:
-                print("-" * 20)
+                print("-"*20)
                 print("Пользователь выиграл!")
                 break
-
+            
             if self.us.board.count == 7:
-                print("-" * 20)
+                print("-"*20)
                 print("Компьютер выиграл!")
                 break
             num += 1
-
+            
     def start(self):
         # self.greet()
         self.loop()
